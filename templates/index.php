7cd906adda2b01ba6df3cc476886e5d8eb307475
Marya <darya.tsvetaeva.96@gmail.com>
2018-11-24 22:26:15 +0300

    Создала шаблон стр. и лейаут

--- templates/index.php
+++ templates/index.php
@@ -17,15 +17,15 @@
             <?php foreach ( $ads  as $key => $val ): ?>
                 <li class="lots__item lot">
                     <div class="lot__image">
-                        <img src="<?= xss($val["img_url"]) ?>" width="350" height="260" alt="">
+                        <img src="<?= esc($val["img_url"]) ?>" width="350" height="260" alt="">
                     </div>
                     <div class="lot__info">
-                        <span class="lot__category"><?= xss($val["category"]) ?></span>
-                        <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?= xss($val["title"]) ?></a></h3>
+                        <span class="lot__category"><?= esc($val["category"]) ?></span>
+                        <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?= esc($val["title"]) ?></a></h3>
                         <div class="lot__state">
                             <div class="lot__rate">
                                 <span class="lot__amount">Стартовая цена</span>
-                                <span class="lot__cost"><?= format_sum(xss($val["price"])) ?></span>
+                                <span class="lot__cost"><?= format_sum(esc($val["price"])) ?></span>
                             </div>
                             <div class="lot__timer timer">
                                 12:23
