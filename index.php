3df44280df1827ad1c6fb3e1c72a890971be07cb
Marya <darya.tsvetaeva.96@gmail.com>
2018-12-05 22:28:36 +0300

    Отправила SQL-запрос для получения списка лотов и категорий

--- index.php
+++ index.php
@@ -1,57 +1,49 @@
 <?php
-require("functions.php");
 $is_auth = rand(0, 1);
 $user_name = "Marya";
 $user_avatar = "img/user.jpg";
-$categories = ["Доски и лыжи", "Крепления", "Ботинки", "Одежда", "Инструменты", "Разное"];
-$ads = [
-    [ 
-        "title" => "2014 Rossignol District Snowboard",
-        "category" => "Доски и лыжи",
-        "price" => "10999",
-        "img_url" => "img/lot-1.jpg"
-    ],
-    [ 
-        "title" => "DC Ply Mens 2016/2017 Snowboard",
-        "category" => "Доски и лыжи",
-        "price" => "159999",
-        "img_url" => "img/lot-2.jpg"
-    ],
-    [ 
-        "title" => "Крепления Union Contact Pro 2015 года размер L/XL",
-        "category" => "Крепления",
-        "price" => "8000",
-        "img_url" => "img/lot-3.jpg"
-    ],
-    [ 
-        "title" => "Крепления Union Contact Pro 2015 года размер L/XL",
-        "category" => "Крепления",
-        "price" => "8000",
-        "img_url" => "img/lot-3.jpg"
-    ],
-    [ 
-        "title" => "Куртка для сноуборда DC Mutiny Charocal",
-        "category" => "Одежда",
-        "price" => "7500",
-        "img_url" => "img/lot-5.jpg"
-    ],
-    [ 
-        "title" => "Маска Oakley Canopy",
-        "category" => "Разное",
-        "price" => "5400",
-        "img_url" => "img/lot-6.jpg"
-    ]
-] ;
+$categories = [];
+$content = "";
+require_once "functions.php";
+require_once "init.php";
 
-$page_content = include_template('index.php', [
-    "ads" => $ads,
-    "categories" => $categories]);
+if (!$content){
+    $sql = "SELECT `id`, `name` FROM categories";
+    $result = mysqli_query($link, $sql);
+    $categories = mysqli_fetch_all($result, MYSQLI_ASSOC );
+}else{
+    $error = mysqli_error($link);
+    $content = include_template('error.php', ["error" => $error]);
+}
+
+
+$sql = "SELECT l.id, l.title, starting_price, img_path, name FROM lots l
+INNER JOIN categories c
+ON l.category_id = c.id
+ORDER BY l.dt_add DESC";
+$result = mysqli_query ($link, $sql);
+
+if ($result){
+    $lots = mysqli_fetch_all($result, MYSQLI_ASSOC );
+}else{
+    $error = mysqli_error($link);
+    $content = include_template('error.php', ["error" => $error]);
+}
+
+
+
+
+if (!$content){
+    $content = include_template('index.php', [
+        "lots" => $lots,
+        "categories" => $categories]);
+}
 
 $layout_content = include_template('layout.php', [
     "title" => 'Yeticave - Главная',
     "is_auth" => $is_auth,
     "user_name" => $user_name,
-    "content" => $page_content,
+    "content" => $content,
     "categories" => $categories]);
 print($layout_content);
 
