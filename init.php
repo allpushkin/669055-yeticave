0675343fca4d9e888038f9e13b08bd58e46a8dc4
Marya <darya.tsvetaeva.96@gmail.com>
2018-12-08 13:52:56 +0300

    Подключения к БД

--- init.php
+++ init.php
@@ -1,7 +1,6 @@
 <?php
 require_once "functions.php";
 $db = require_once "config/db.php";
-$categories = [];
 
 $link = mysqli_connect($db["host"],$db["user"],$db["password"],$db["database"]);
 mysqli_set_charset($link,"utf8");
@@ -14,7 +13,7 @@ if (!$link) {
     "is_auth" => $is_auth,
     "user_name" => $user_name,
     "content" => $content,
-    "categories" => $categories]);
+    "categories" => []]);
 print($layout_content);
 exit;
 }
