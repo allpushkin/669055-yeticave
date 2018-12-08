9cb87b5094eec0f68965c1770bc4ecf8342b1e13
Marya <darya.tsvetaeva.96@gmail.com>
2018-12-08 15:16:02 +0300

    Подключения к БД

--- init.php
+++ init.php
@@ -3,7 +3,6 @@ require_once "functions.php";
 $db = require_once "config/db.php";
 
 $link = mysqli_connect($db["host"],$db["user"],$db["password"],$db["database"]);
-mysqli_set_charset($link,"utf8");
 
 if (!$link) {
     $error = mysqli_connect_error();
@@ -18,4 +17,5 @@ print($layout_content);
 exit;
 }
 
+mysqli_set_charset($link,"utf8");
 ?>
