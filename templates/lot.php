a4eea50c088b8fbc4938a2f9a4475a37ec706df1
Marya <darya.tsvetaeva.96@gmail.com>
2018-12-10 19:03:28 +0300

    Создала новый шаблон для показа страницы лота

--- templates/lot.php
+++ templates/lot.php
@@ -1,34 +1,25 @@
 <section class="lot-item container">
-  <h2><?= esc($lots["title"]); ?></h2>
+  <h2><?= esc($lot["title"]); ?></h2>
   <div class="lot-item__content">
     <div class="lot-item__left">
       <div class="lot-item__image">
-        <img src="<?= $lots["img_path"] ?>" width="730" height="548" alt="Сноуборд">
+        <img src="<?= $lot["img_path"] ?>" width="730" height="548" alt="Сноуборд">
       </div>
-      <p class="lot-item__category">Категория: <span><?= esc($categories["name"])?></span></p>
-      <p class="lot-item__description">Легкий маневренный сноуборд, готовый дать жару в любом парке, растопив
-        снег
-        мощным щелчкоми четкими дугами. Стекловолокно Bi-Ax, уложенное в двух направлениях, наделяет этот
-        снаряд
-        отличной гибкостью и отзывчивостью, а симметричная геометрия в сочетании с классическим прогибом
-        кэмбер
-        позволит уверенно держать высокие скорости. А если к концу катального дня сил совсем не останется,
-        просто
-        посмотрите на Вашу доску и улыбнитесь, крутая графика от Шона Кливера еще никого не оставляла
-        равнодушным.</p>
+      <p class="lot-item__category">Категория: <span><?= esc($lot["category_name"])?></span></p>
+      <p class="lot-item__description"><?= esc($lot["description"]); ?></p>
     </div>
     <div class="lot-item__right">
       <div class="lot-item__state">
         <div class="lot-item__timer timer">
-          10:54
+          <?= lot_time_left(); ?>
         </div>
         <div class="lot-item__cost-state">
           <div class="lot-item__rate">
             <span class="lot-item__amount">Текущая цена</span>
-            <span class="lot-item__cost">10 999</span>
+            <span class="lot-item__cost"><?= format_sum(esc($lot["starting_price"])) ?></span>
           </div>
           <div class="lot-item__min-cost">
-            Мин. ставка <span>12 000 р</span>
+            Мин. ставка <span><?= format_sum(esc($lot["price"])) ?></span>
           </div>
         </div>
         <form class="lot-item__form" action="https://echo.htmlacademy.ru" method="post">
