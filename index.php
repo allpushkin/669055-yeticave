2e3826b0dce751c774e07edbffe529306331dbdd
Marya <darya.tsvetaeva.96@gmail.com>
2018-12-10 23:18:49 +0300

    Отправила SQL-запрос для получения списка лотов и категорий

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 require_once "functions.php";
 require_once "init.php";
 
-$is_auth = rand(0, 1);
-$user_name = "Marya";
+$is_auth     = rand(0, 1);
+$user_name   = "Marya";
 $user_avatar = "img/user.jpg";
 
 $categories = fetch_data($link, "SELECT `id`, `name` FROM categories");
@@ -14,16 +14,16 @@ ON l.category_id = c.id
 ORDER BY l.dt_add DESC");
 
 $page_content = include_template('index.php', [
-    "lots" => $lots,
-    "categories" => $categories
+   "lots"       => $lots,
+   "categories" => $categories
 ]);
 
 $layout_content = include_template('layout.php', [
-    "title" => 'Yeticave - Главная',
-    "is_auth" => $is_auth,
-    "user_name" => $user_name,
-    "content" => $page_content,
-    "categories" => $categories
+   "title"      => 'Yeticave - Главная',
+   "is_auth"    => $is_auth,
+   "user_name"  => $user_name,
+   "content"    => $page_content,
+   "categories" => $categories
 ]);
 print($layout_content);
 
