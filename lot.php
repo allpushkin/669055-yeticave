ef4e3bdd0d6afdd990e947fd69cff88e341a18cf
Marya <darya.tsvetaeva.96@gmail.com>
2018-12-11 21:00:40 +0300

    Создала новый сценарий для показа страницы лота

--- lot.php
+++ lot.php
@@ -9,25 +9,26 @@ $user_avatar = "img/user.jpg";
 $categories = fetch_data($link, "SELECT `id`, `name` FROM categories");
 
 if (isset($_GET['id'])) {
-    $lot_id = $_GET['id'];
+    $lot_id = mysqli_real_escape_string($link, $_GET['id']);
 } else {
     http_response_code(404);
     $page_content = include_template('404.php', ['categories' => $categories]);
 }
 
-$lot = fetch_data($link, "SELECT l.id, l.title, starting_price, img_path, description, price, name as category_name FROM lots l
+
+$lots = fetch_data($link, "SELECT l.id, l.title, starting_price, img_path, description, price, name as category_name FROM lots l
 LEFT JOIN bets b
 ON l.id = b.lot_id
 INNER JOIN categories c
 ON l.category_id = c.id
-WHERE l.id = $lot_id ");
+WHERE l.id = $lot_id");
 
-if(!isset($lot[0]['id'])) {
+if(!isset($lots[0]['id'])) {
     http_response_code(404);
     $page_content = include_template('404.php', ['categories' => $categories]);
 } else {
     $page_content = include_template('lot.php', [
-        "lot"        => $lot[0],
+        "lot"        => $lots[0],
         "categories" => $categories
 ]);
 }
