5790ff45a7b02fb8964139fa2aef2478e6a50f1c
Marya <darya.tsvetaeva.96@gmail.com>
2018-12-16 22:45:58 +0300

    Ошибка,доступ запрещен 403

---
+++ templates/403.php
@@ -0,0 +1,14 @@
+<nav class="nav">
+    <ul class="nav__list container">
+        <!--список из массива категорий-->
+        <?php foreach ($categories as $category): ?>
+            <li class="nav__item">
+                <a href="pages/all-lots.html"><?=$category['name']?></a>
+            </li>
+        <?php endforeach; ?>
+    </ul>
+ </nav>
+ <section class="lot-item container">
+    <h2>403 Доступ запрещен</h2>
+    <p>Доступ к данной странице запрещен.</p>
+</section> 
