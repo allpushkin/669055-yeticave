babccf1b9e7ed6fbeacf9da620e197476d52c082
Marya <darya.tsvetaeva.96@gmail.com>
2018-12-18 16:38:17 +0300

    Новая функция жизни лота и функция опред. времени ставки

--- functions.php
+++ functions.php
@@ -16,7 +16,6 @@ function include_template($name, $data) {
     return $result;
 }
 
-
 function format_sum($sum){
     $sum = ceil($sum);
     if ($sum >= 1000) {
@@ -31,47 +30,19 @@ function esc ($arg){
     return htmlspecialchars($arg);
 }
 
+function lotTimeLeft($data) { 
+    $now      = date_create("now"); 
+    $data     = date_create($data);
 
-function lot_time_left() { 
-    $now      = date_create("now");
-    $tomorrow = date_create("tomorrow");
-
-    $diff     = date_diff($now, $tomorrow);
-    return date_interval_format($diff,"%H:%I");
+    $diff     = date_diff($now, $data);
+    return ($diff->days * 24) + $diff->h . ":" . $diff->i ;
 } 
-
-
-
-function lotTimeLeft($expiration_dt) {
-    $time_left = strtotime($expiration_dt) - time();
-    $hours     = floor($time_left / 3600);
-    $minutes   = floor(($time_left % 3600) / 60);
-    if ($minutes < 10) {
-        $minutes = 0 . $minutes;
-    }
-    if ($hours < 10) {
-        $hours = 0 . $hours;
-    }
-    $time_left = $hours . ':' . $minutes;
-    return $time_left;
-}
-
-
 
 function fetch_data($link,$sql) {
     $result = mysqli_query ($link, $sql);
 
     return mysqli_fetch_all($result, MYSQLI_ASSOC );
 }
-
-
-function add ($link, $lot){
-    foreach ($lot as & $add_lot) {
-        $add_lot =mysqli_real_escape_string($link, $add_lot);
-    }
-    return implode("', '", $lot);
-}
-
 
 function db_get_prepare_stmt($link, $sql, $data = []) {
     $stmt = mysqli_prepare($link, $sql);
@@ -108,11 +79,9 @@ function db_get_prepare_stmt($link, $sql, $data = []) {
     return $stmt;
 }
 
-
 function isFormError($errors, $key) {
   return isset($errors[$key]) ? true : false;
 }
-
 
 function time_passed($date) {
     $interval = strtotime('now') - strtotime($date);
@@ -125,11 +94,10 @@ function time_passed($date) {
     if ($interval > 60 && $interval < 3600) {
         return floor($interval / 60) . ' минут назад';
     }
-    else {
-        return 'меньше минуты назад';
-    }
+
+    return 'меньше минуты назад';
 }
 
 
-
 ?>
+
